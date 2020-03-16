1017512abb5e5a702bbe40861b3e2120ab82e8e7
= <benzastawnik@gmail.com>
2020-03-16 07:14:46 -0400

    Move logging configuration so that child loggers respect logging level

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 import discord
-import logging
 from config import config
+import logging
+if config["debug_mode"]:
+	logging.basicConfig(level = logging.INFO)
+else:
+	logging.basicConfig(level = logging.WARNING)
 import commands
 
 _emojiBotMasterLogger = logging.getLogger("EmojiBot")
@@ -42,8 +46,4 @@ class EmojiBotClient(discord.Client):
 
 if __name__ == "__main__":
 	clientInstance = EmojiBotClient()
-	if config["debug_mode"]:
-		logging.basicConfig(level = logging.INFO)
-	else:
-		logging.basicConfig(level = logging.WARNING)
 	clientInstance.run(config["discord_client_key"])
